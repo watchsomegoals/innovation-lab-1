c92d4dcd8d620d9c8225554ce062326e34df8673
Valentin Caraba <if18b122@technikum-wien.at>
2021-01-27 04:35:01 +0100

    Racing Game

---
+++ Assets/Scripts/CarChoice.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarChoice : MonoBehaviour
+{
+    public GameObject RedBody;
+    public GameObject BlueBody;
+    public int CarImport; //1=Red, 2=Blue
+
+    void Start()
+    {
+        CarImport = GlobalCar.CarType;
+        if(CarImport == 1)
+        {
+            RedBody.SetActive(true);
+        }
+
+        if(CarImport == 2)
+        {
+            BlueBody.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Vehicles.Car;
+
+public class Countdown : MonoBehaviour
+{
+    public GameObject CountDown;
+    public GameObject LapTimer;
+    public GameObject CarControl;
+    public GameObject CarControlOther;
+    public AudioSource LevelMusic;
+
+    void Start()
+    {
+        StartCoroutine(CountStart());
+    }
+
+    IEnumerator CountStart()
+    {
+        yield return new WaitForSeconds(0.5f);
+        CountDown.GetComponent<Text>().text = "3";
+        CountDown.SetActive(true);
+        yield return new WaitForSeconds(1);
+        CountDown.SetActive(false);
+        CountDown.GetComponent<Text>().text = "2";
+        CountDown.SetActive(true);
+        yield return new WaitForSeconds(1);
+        CountDown.SetActive(false);
+        CountDown.GetComponent<Text>().text = "1";
+        CountDown.SetActive(true);
+        yield return new WaitForSeconds(1);
+        CountDown.SetActive(false);
+        CountDown.GetComponent<Text>().text = "GO";
+        CountDown.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        CountDown.SetActive(false);
+
+        //LevelMusic.Play();
+
+        LapTimer.SetActive(true);
+
+        CarControl.GetComponent<CarController>().enabled = true;
+        CarControlOther.GetComponent<CarAIControl>().enabled = true;
+    }
+}
